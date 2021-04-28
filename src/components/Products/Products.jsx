b1a77969bc68b195be9c084bf8cd5cb78ec6b958
Zeeshan <jawedzeeshan1@gmail.com>
2021-04-28 07:36:47 +0500

    Update Products.jsx

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,14 +1,36 @@
-import { Card, CardContent, Grid } from "@material-ui/core"
+import { Card, CardActionArea, CardContent, CardMedia, Grid, Typography } from "@material-ui/core"
 import ShoesDetails from '../../data/ShoesDetails'
 
 const Products = () => {
+    const images = require.context("../../", true);
     return (
         <Grid container spacing={2}>
            
                 
                 {ShoesDetails.map((shoe)=><Grid item xs={6}
 			sm={8}
-			md={4}> <Card> <CardContent> {shoe.name}</CardContent></Card></Grid>)}
+			md={4}> <Card> <CardActionArea>
+            <CardMedia
+                component="img"
+                alt={shoe.name}
+                height="300"
+                image={
+                    images(`.${shoe.images[0]}`).default
+                }
+                title={shoe.name}
+            />
+            <CardContent>
+                <Typography
+                    gutterBottom
+                    variant="h5"
+                    component="h2"
+                    // className={classes.homeCardName}
+                >
+                    {shoe.name}
+                </Typography>
+                </CardContent>
+                </CardActionArea>
+                </Card></Grid>)}
                
             
             {/* <Grid item xs={12}
